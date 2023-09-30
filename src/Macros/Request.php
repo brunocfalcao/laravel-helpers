f69fc0fa01f75e2571a9953957feaecc661d6c64
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-30 21:46:24 +0200

    Public IP refactored, and NovaHelpers Trait

--- src/Macros/Request.php
+++ src/Macros/Request.php
@@ -2,11 +2,22 @@
 
 use Illuminate\Http\Request;
 
-Request::macro('ip2', function () {
+/**
+ * Super nice new IP that returns your localhost public IP instead of
+ * returning your localhost IP.
+ */
+Request::macro('public_ip', function () {
     try {
-        return request()->ip() == '127.0.0.1' ?
-        file_get_contents('https://ipinfo.io/ip') :
-        request()->ip();
+        $ip = request()->ip();
+
+        // Check for forwarded IP address
+        if (request()->header('x-forwarded-for')) {
+            $ip = explode(',', request()->header('x-forwarded-for'))[0];
+        }
+
+        return $ip === '127.0.0.1' ?
+            trim(file_get_contents('https://ipinfo.io/ip')) :
+            $ip;
     } catch (\Exception $ex) {
         return request()->ip();
     }
