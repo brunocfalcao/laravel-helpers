c974c81b366aecdc60742b4a8e7f08e1d3b5a4b3
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-01-06 01:25:30 +0100

    Update HasCanonicals.php

--- src/Traits/HasCanonicals.php
+++ src/Traits/HasCanonicals.php
@@ -9,7 +9,7 @@ trait HasCanonicals
     public function upsertCanonical($model, string $value, string $attribute = 'canonical')
     {
         // Check if the attribute is dirty or blank
-        if ($model->isDirty($attribute) || empty($model->$attribute)) {
+        if (blank($model->$attribute)) {
             // Convert the value to kebab case
             $canonicalValue = Str::slug($value, '-');
 
