25643541c5a21709644c5d32666ca9cce1946049
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-21 23:54:12 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,27 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+
+if (! function_exists('log_queries')) {
+    function log_queries()
+    {
+        // Listen to database queries
+        DB::listen(function ($query) {
+            // Combine the query with its bindings
+            $fullQuery = vsprintf(str_replace(['%', '?'], ['%%', "'%s'"], $query->sql), $query->bindings);
+
+            // Log the full query
+            info($fullQuery);
+        });
+    }
+
+    function stop_logging_queries()
+    {
+        DB::disableQueryLog();
+    }
+}
 
 if (! function_exists('run')) {
 
